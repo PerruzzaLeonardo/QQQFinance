56a9d1648ba2229f9eeddce3e45a6cd00f639535
PerruzzaLeonardo <perruzza.1884263@studenti.uniroma1.it>
2023-03-08 23:00:46 +0100

    add google login

--- code/app/controllers/users/sessions_controller.rb
+++ code/app/controllers/users/sessions_controller.rb
@@ -15,7 +15,7 @@ class Users::SessionsController < Devise::SessionsController
 
 
   def create #super
-    self.resource = warden.authenticate!
+    self.resource = warden.authenticate!(auth)
     set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
     yield resource if block_given?
--- code/config/initializers/devise.rb
+++ code/config/initializers/devise.rb
@@ -9,6 +9,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -25,6 +28,9 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'QQQFinanceASSI@gmail.com'
+  config.omniauth :google_oauth2, 
+  Rails.application.credentials.dig(:GOOGLE_OAUTH_CLIENT_ID),
+  Rails.application.credentials.dig(:GOOGLE_OAUTH_CLIENT_SECRET)
 
   # Configure the class responsible to send e-mails.
    config.mailer = 'Devise::Mailer'
