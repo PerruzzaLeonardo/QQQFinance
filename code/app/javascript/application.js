4ac56226a7901c990165b42e5e34c649c28c32bc
DBuzz20 <davidebuzzichini@libero.it>
2023-03-14 14:08:35 +0100

    ordinamento tabella

--- code/app/javascript/application.js
+++ code/app/javascript/application.js
@@ -1,4 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+//= require jquery
+//= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+
 
