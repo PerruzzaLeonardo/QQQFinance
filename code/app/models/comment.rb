c601e673594ed566e362fa57592ce453c993d2a0
DBuzz20 <davidebuzzichini@libero.it>
2023-03-27 14:05:35 +0200

    rspec+minor fix

--- code/app/models/comment.rb
+++ code/app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :azione
 
-    validates :azione_id,:commento,:user_id, presence: true 
+    validates :azione_id,:commento,:user_id, presence: true
 end
--- code/spec/controllers/wallets_controller_spec.rb
+++ code/spec/controllers/wallets_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe WalletsController, type: :controller do
 
   before(:all) do
-    @user = FactoryBot.create(:user)
-    #@wallet= FactoryBot.create(:wallet)
+    @user=FactoryBot.create(:user)
+    @wallet= FactoryBot.create(:wallet)
     @azione= FactoryBot.create(:azione)
   end
 
@@ -12,17 +12,27 @@ RSpec.describe WalletsController, type: :controller do
     @user.destroy
   end
 
-  describe "aggiunta al wallet" do
+  describe "POST #movimenti" do
   #  it "aggiunge un'azione al wallet" do
    #   post :create, params: { wallet: { user: user.username, azione: "MSFT", quantità: 23 } }
     #  expect(response).to redirect_to(wallet_path(assigns(:wallet)))
      # expect(assigns(:wallet).azione).to eq("MSFT")
     #end
+    context 'con isin corretto' do
+      it 'aggiunge correttamente azione al wallet' do
+        allow(controller).to receive(:authenticate_user!).and_return(true)
+        expect{post :movimenti,params:{isin:'ABNB',quantità:5,commit:'aggiungi'}}.to change(Wallet, :count).by(1)
+        expect(response).to redirect_to("/wallet")
+        expect(flash[:notice]).to be_present
+      end
+    end
 
-    it "isin non valido" do
-      get :movimenti, params: {isin:'ABCD',quantità:5}
-
-      expect(response).to have_content('ABCD non è un isin valido') 
+    context 'con isin non valido' do
+      it 'non aggiunge azione al wallet' do
+        expect{post :movimenti,params:{isin:'ABCD',quantità:5,commit:'aggiungi'}}.not_to change(Wallet, :count)
+        expect(response).to redirect_to("/wallet")
+        expect(flash[:notice]).to be_present
+      end
     end
   end
 end
