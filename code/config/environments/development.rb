6caf050afe0cf78797ae83f939e0fc3d8f814961
PerruzzaLeonardo <perruzza.1884263@studenti.uniroma1.it>
2023-03-29 00:58:37 +0200

    last security commit

--- code/config/environments/development.rb
+++ code/config/environments/development.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
       :address              => 'smtp.gmail.com',
       :port                 => 587,
       :domain               => 'gmail.com',
-      :user_name            => 'qqqfinanceassi@gmail.com',
+      :user_name            =>  Rails.application.credentials.dig(:EMAIL),
       :password             =>  Rails.application.credentials.dig(:PASSWORD),
       :authentication       => 'plain',
       :enable_starttls_auto => true
